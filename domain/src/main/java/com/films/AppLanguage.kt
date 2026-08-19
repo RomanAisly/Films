adfb242e575b4824341024cd789f2d783d4c6c5c
RomanAisly <romanalexaisly@gmail.com>
2026-08-19 12:35:57 +0300

    fix language autochange

--- domain/src/main/java/com/films/AppLanguage.kt
+++ domain/src/main/java/com/films/AppLanguage.kt
@@ -1,11 +1,11 @@
 package com.films
 
 enum class AppLanguage(val localeCode: String) {
-    ENGLISH("en-US"),
-    RUSSIAN("ru-RU"),
-    CHINESE("zh-CN"),
-    SPANISH("es-ES"),
-    FRENCH("fr-FR"),
-    GERMAN("de-DE"),
-    UKRAINIAN("uk-UA")
+    ENGLISH("en"),
+    RUSSIAN("ru"),
+    CHINESE("zh"),
+    SPANISH("es"),
+    FRENCH("fr"),
+    GERMAN("de"),
+    UKRAINIAN("uk")
 }
